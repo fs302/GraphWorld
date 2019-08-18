d0d9797f4f91ce0585de4179aad903c7abc27fa1
亿想 <fanshen.fs@alibaba-inc.com>
2019-08-18 14:08:22 +0800

    graph classification with Graph Isomorphism Networks

--- common/data_utils.py
+++ common/data_utils.py
@@ -6,7 +6,10 @@ data_file = {"facebook": "facebook/facebook.txt",
              "BlogCatalog": "BlogCatalog/edges_format2.txt",
              "lyb": "lyb/lyb.e",
              "USAir": "USAir/USAir.txt",
-             "twitter": "twitter/twitter.txt"
+             "twitter": "twitter/twitter.txt",
+             "MUTAG": "MUTAG/MUTAG.txt",
+             "IMDBBINARY": "IMDBBINARY/IMDBBINARY.txt",
+             "PTC": "PTC/PTC.txt"
              }
 
 
